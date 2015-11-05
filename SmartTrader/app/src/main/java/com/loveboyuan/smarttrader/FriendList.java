2d1bf07e675d3e9904a9fc7b3c79d9a3c2a3a2f0
Jiahui Xie <xtom32@gmail.com>
2015-11-04 19:50:40 -0700

    Added observer list to FriendList

--- SmartTrader/app/src/main/java/com/loveboyuan/smarttrader/FriendList.java
+++ SmartTrader/app/src/main/java/com/loveboyuan/smarttrader/FriendList.java
@@ -5,22 +5,33 @@ import java.util.ArrayList;
  * Created by jiahui on 10/18/15.
  */
 public class FriendList {
-    private ArrayList<Profile> friendList;
+    private ArrayList<Profile> friendList = new ArrayList<Profile>();
+    private ArrayList<MyObserver> observers = new ArrayList<MyObserver>();
 
-    public FriendList() {
-        this.friendList = new ArrayList<Profile>(1);
-    }
-
-    public void add(Profile profile) {
+    public void addFriend(Profile profile) {
         if (false == this.friendList.contains(profile)) {
             this.friendList.add(profile);
         }
+        this.notifyAllObservers();
     }
 
-    public void remove(Profile profile) {
+    public void removeFriend(Profile profile) {
         if (this.friendList.contains(profile)) {
             this.friendList.remove(profile);
         }
+        this.notifyAllObservers();
     }
 
+    public ArrayList<Profile> getFriendList() {
+        return friendList;
+    }
+
+    public void addMyObserver(MyObserver observer) {
+        observers.add(observer);
+    }
+
+    public void notifyAllObservers() {
+        for (MyObserver observer : observers)
+            observer.update();
+    }
 }
